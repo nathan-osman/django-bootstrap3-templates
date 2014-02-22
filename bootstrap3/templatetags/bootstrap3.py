e6353a9ea8b6ef656a0077154aefda6b78301846
Nathan Osman <admin@quickmediasolutions.com>
2014-02-22 14:11:55 -0800

    Added widget_type template filter and modified form_control to properly process MultiWidgets.

--- bootstrap3/templatetags/bootstrap3.py
+++ bootstrap3/templatetags/bootstrap3.py
@@ -1,9 +1,20 @@
-from django import template
+from django import forms, template
 
 register = template.Library()
 
-@register.filter
-def form_control(field):
-    """Add the form-control CSS class to a field."""
-    field.field.widget.attrs.update({'class': 'form-control',})
+@register.simple_tag
+def render_field(field, **kwargs):
+    """Render the field with the specified attributes."""
+    if isinstance(field.field.widget, forms.MultiWidget):
+        def format_output(rendered_widgets):
+            width = max(12 / len(rendered_widgets), 3)
+            cols = ('<div class="col-xs-%d">%s</div>' % (width, w,) for w in rendered_widgets)
+            return '<div class="row">%s</div>' % ''.join(cols)
+        field.field.widget.format_output = format_output
+    field.field.widget.attrs.update(kwargs)
     return field
+
+@register.filter
+def widget_type(field):
+    """Return the widget type of the field."""
+    return field.field.widget.__class__.__name__.lower()
