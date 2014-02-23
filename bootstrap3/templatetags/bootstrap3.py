c3ee4e789644a861f0a60403dcdeefe9b885d8bc
Nathan Osman <admin@quickmediasolutions.com>
2014-02-22 19:12:43 -0800

    Reorganized form.html template to render nearly every widget correctly.

--- bootstrap3/templatetags/bootstrap3.py
+++ bootstrap3/templatetags/bootstrap3.py
@@ -3,15 +3,18 @@ from django import forms, template
 register = template.Library()
 
 @register.simple_tag
-def render_field(field, **kwargs):
-    """Render the field with the specified attributes."""
+def render_field(field):
+    """Render the field."""
     if isinstance(field.field.widget, forms.MultiWidget):
         def format_output(rendered_widgets):
             width = max(12 / len(rendered_widgets), 3)
             cols = ('<div class="col-xs-%d">%s</div>' % (width, w,) for w in rendered_widgets)
             return '<div class="row">%s</div>' % ''.join(cols)
         field.field.widget.format_output = format_output
-    field.field.widget.attrs.update(kwargs)
+    if not issubclass(field.field.__class__, forms.FileField):
+        field.field.widget.attrs.update({
+            'class': 'form-control',
+        })
     return field
 
 @register.filter
